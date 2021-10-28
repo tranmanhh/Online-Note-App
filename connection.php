55df2ab4ca952e52d05fc1a63c71e8041336164a
Manh Dinh <70175969+tranmanhh@users.noreply.github.com>
2021-10-28 16:26:08 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $DB = new mysqli("remotemysql.com", "ntxgCa0F76", "hwbZiF5uqP", "ntxgCa0F76");
+    $DB = new mysqli("path", "username", "password", "databasename");
     if($DB->errno > 0)
     {
         die("<div class='alert alert-danger'>ERROR: Unable to connect to database " . $DB->error . "</div>");
